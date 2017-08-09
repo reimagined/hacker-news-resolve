fc8b135ed2602d17028bfc7b1aa453b70098e7e8
Vasiliy <vasiliy.lavrov@devexpress.com>
2017-08-09 19:52:10 +0300

    Implement simple item

--- client/components/NewsComponent.js
+++ client/components/NewsComponent.js
@@ -1,47 +1,54 @@
 import React from 'react';
 import url from 'url';
+import { Link } from 'react-router-dom';
 
-const NewsItem = ({ index, id, caption, link, user, points, comments }) => {
-    return <div className="Item">
-      {caption}
+const isExternalLink = link => link[0] !== '/';
+
+function getHostname(link) {
+    const urlObj = url.parse(link); //TODO: check is empty
+    return (urlObj.hostname.split('.')[0] === 'www') ? urlObj.hostname.substr(4) : urlObj.hostname;
+};
+
+const ItemTitle = ({ title, link }) => {
+    if (isExternalLink(link)) {
+        return <div className="Item__title">
+                <a href={link}>{title}</a> <span className="Item__host">({getHostname(link)})</span>
+            </div>;
+    }
+    return <div className="Item__title">
+            <Link to={link}>{title}</Link>
+        </div>;
+};
+
+
+const ItemMeta = ({ score }) => {
+    return <div className="Item__meta">
+      <span className="Item__score">
+        {score} points
+      </span>
     </div>;
-}
+};
 
-// <div className="Item__content">
-//         {caption}
-//         {this.renderItemMeta(item, (threadStore.lastVisit !== null && threadStore.newCommentCount > 0 && <span>{' '}
-//           (<em>{threadStore.newCommentCount} new</em> in the last <TimeAgo date={threadStore.lastVisit} formatter={timeUnitsAgo}/>{') | '}
-//           <span className="control" tabIndex="0" onClick={this.autoCollapse} onKeyPress={this.autoCollapse} title="Collapse threads without new comments">
-//             auto collapse
-//           </span>{' | '}
-//           <span className="control" tabIndex="0" onClick={this.markAsRead} onKeyPress={this.markAsRead}>
-//             mark as read
-//           </span>
-//         </span>))}
-//         {item.text && <div className="Item__text">
-//           <div dangerouslySetInnerHTML={{__html: item.text}}/>
-//         </div>}
-//         {item.type === 'poll' && <div className="Item__poll">
-//           {item.parts.map(function(id) {
-//             return <PollOption key={id} id={id}/>
-//           })}
-//         </div>}
-//       </div>
-//        {item.kids && <div className="Item__kids">
-//         {item.kids.map(function(id, index) {
-//           return <Comment key={id} id={id} level={0}
-//             loadingSpinner={index === 0}
-//             threadStore={threadStore}
-//           />
-//         })}
-//       </div>} 
+const NewsItem = ({ title, link, user, score, commentCount }) => {
+    return <li className="ListItem">
+        <div className="Item">
+            <div className="Item__content">
+                <ItemTitle title={title} link={link} />
+                <ItemMeta user={user} score={score} commentCount={commentCount} />
+            </div>
+        </div>
+    </li>;
+};
 
 const NewsComponent = ({ items }) => {
     return <div>
-            <h1>News</h1>
-            <NewsItem index="1" id="1" caption="Event Sourcing" link="https://martinfowler.com/eaaDev/EventSourcing.html" user="admin" points="777" comments="777" />
-            <NewsItem index="2" id="2" caption="Offline GraphQL Queries with Redux Offline and Apollo" link="http://www.east5th.co/blog/2017/07/24/offline-graphql-queries-with-redux-offline-and-apollo/" user="admin" points="10" comments="1" />
-        </div>;
-}
+        <div className="Items">
+            <ol className="Items__list" start="1">
+                <NewsItem title="Event Sourcing" link="https://martinfowler1.com/eaaDev/EventSourcing.html" user="admin" score="777" comments="777" />
+                <NewsItem title="Offline GraphQL Queries with Redux Offline and Apollo" link="http://www.east5th1.co/blog/2017/07/24/offline-graphql-queries-with-redux-offline-and-apollo/" user="admin" score="10" comments="1" />
+            </ol>
+        </div>
+    </div>;
+};
 
-export default NewsComponent
+export default NewsComponent;
