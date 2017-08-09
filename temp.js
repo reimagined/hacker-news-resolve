8a38f8cc271911d70a7e8272d5c498f30761cc74
mrcheater <slayerx@narod.ru>
2017-08-09 18:43:14 +0300

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,5 +1,5 @@
 const a = (function () {
-    const [q,w,e,r,t,y,u,i,o, p,a ,s,d, f,g,h,j,k,l,z,x,c,v,b,n,m] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+    const [q,w,e,r,t, y,u,i ,o, p,a ,s,d, f,g,h,j,k,l,z,x,c,v,b,n,m] = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
     return [q,w,e,r,t,y,u,i,o,p,a,s,d,f,g,h,j,k,l,z,x,c,v,b,n,m]
 
 })
