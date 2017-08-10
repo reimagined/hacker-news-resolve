51aea8000a98bf2510d30d2d0ed65829791adee9
mrcheater <slayerx@narod.ru>
2017-08-10 18:14:58 +0300

    Update reducers

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
+import user from './user';
 import users from './users';
 import comments from './comments';
+import news from './news';
 
 export default combineReducers({
+    user,
     users,
-    comments
+    comments,
+    news
 });
