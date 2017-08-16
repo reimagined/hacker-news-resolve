3c2e0eeac3e77ba2033ed508c208e4c282e02d5e
kilimondjaro <kirill.babich@mail.ru>
2017-08-16 12:03:27 +0300

    add expand feature

--- client/components/CommentComponent.js
+++ client/components/CommentComponent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 const getLevelClassName = level => {
@@ -7,72 +7,90 @@ const getLevelClassName = level => {
   return `Comment--level${level > 0 ? level : '0'}`;
 };
 
-const ExpandButton = ({ expanded }) => {
+const ExpandButton = ({ expanded, onClick }) => {
   return (
-    <span className="Comment__collapse" tabindex="0">
+    <span onClick={onClick} className="Comment__collapse" tabindex="0">
       [{expanded ? '-' : '+'}]
     </span>
   );
 };
 
-const CommentComponent = ({
-  id,
-  level,
-  content,
-  user,
-  date,
-  expanded,
-  getChilrenCallback,
-  showReply,
-  replies,
-  parent,
-  root
-}) => {
-  return (
-    <div>
-      <div
-        className={`Comment ${getLevelClassName(level)} ${expanded
-          ? null
-          : 'Comment--collapsed'}`}
-      >
-        <div className="Comment__content">
-          <div className="Comment__meta">
-            <ExpandButton expanded={expanded} />
-            <span>
-              {' '}<Link className="Comment__user" to={`/user/${user}`}>
-                {user}
-              </Link>
-            </span>
-            <span>
-              {' '}<time>{date.toLocaleString('en-US')}</time>
-            </span>
-            <span>
-              {' '}| <Link to={`/comment?id=${id}`}>link</Link>
-            </span>
-            {parent &&
+class CommentComponent extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      expanded: true
+    };
+  }
+  render() {
+    const {
+      id,
+      level,
+      content,
+      user,
+      date,
+      expanded,
+      getChilrenCallback,
+      showReply,
+      replies,
+      parent,
+      root
+    } = this.props;
+
+    return (
+      <div>
+        <div
+          className={`Comment ${getLevelClassName(level)} ${expanded
+            ? null
+            : 'Comment--collapsed'}`}
+        >
+          <div className="Comment__content">
+            <div className="Comment__meta">
+              <ExpandButton
+                onClick={() =>
+                  this.setState({ expanded: !this.state.expanded })}
+                expanded={this.state.expanded}
+              />
               <span>
-                {' '}| <Link to={parent}>parent</Link>
-              </span>}
-            {root &&
+                {' '}<Link className="Comment__user" to={`/user/${user}`}>
+                  {user}
+                </Link>
+              </span>
               <span>
-                {' '}| on: <Link to={`/story?id=${root.id}`}>{root.title}</Link>
-              </span>}
-          </div>
-          <div className="Comment__text">
-            <div>
-              {content}
+                {' '}<time>{date.toLocaleString('en-US')}</time>
+              </span>
+              <span>
+                {' '}| <Link to={`/comment?id=${id}`}>link</Link>
+              </span>
+              {parent &&
+                <span>
+                  {' '}| <Link to={parent}>parent</Link>
+                </span>}
+              {root &&
+                <span>
+                  {' '}| on:{' '}
+                  <Link to={`/story?id=${root.id}`}>{root.title}</Link>
+                </span>}
             </div>
-            <p>
-              {showReply && <Link to={`/reply?id=${id}`}>reply</Link>}
-            </p>
+            {this.state.expanded &&
+              <div className="Comment__text">
+                <div>
+                  {content}
+                </div>
+                <p>
+                  {showReply && <Link to={`/reply?id=${id}`}>reply</Link>}
+                </p>
+              </div>}
           </div>
         </div>
+        {getChilrenCallback
+          ? this.state.expanded &&
+            getChilrenCallback({ replies, level: level ? level + 1 : 1 })
+          : null}
       </div>
-      {getChilrenCallback
-        ? getChilrenCallback({ replies, level: level ? level + 1 : 1 })
-        : null}
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default CommentComponent;
