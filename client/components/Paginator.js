c2a009a4b23a3f026e6f36c4640a28955263ae04
mrcheater <slayerx@narod.ru>
2017-08-17 17:21:20 +0300

    Add tests/components/Paginator

--- client/components/Paginator.js
+++ client/components/Paginator.js
@@ -3,38 +3,41 @@ import { Link } from 'react-router-dom';
 
 import '../styles/paginator.css';
 
-const Paginator = ({ page = 1, hasNext, location }) => {
-  const route = location.pathname;
-  const onClick = () =>
-    setTimeout(function() {
-      window.scrollTo(0, 0);
-    }, 0);
+class Paginator extends React.PureComponent {
+  scrollUp = () => {
+    window.scrollTo(0, 0);
+  };
 
-  if (page === 1 && !hasNext) {
-    return null;
+  render() {
+    const { page = 1, hasNext, location } = this.props;
+    const { pathname } = location;
+
+    if (page === 1 && !hasNext) {
+      return null;
+    }
+    const nextDisabledClassName = !hasNext && 'paginator__disabled';
+    const prevDisabledClassName = page <= 1 && 'paginator__disabled';
+
+    return (
+      <div className="paginator">
+        <Link
+          className={prevDisabledClassName}
+          to={`${pathname}?page=${Number(page) - 1}`}
+          onClick={this.scrollUp}
+        >
+          <span className={prevDisabledClassName}>Prev</span>
+        </Link>
+        {` | ${page} | `}
+        <Link
+          className={nextDisabledClassName}
+          to={`${pathname}?page=${Number(page) + 1}`}
+          onClick={this.scrollUp}
+        >
+          <span className={nextDisabledClassName}>More</span>
+        </Link>
+      </div>
+    );
   }
-  const nextDisabledClassName = !hasNext && 'paginator__disabled';
-  const prevDisabledClassName = page <= 1 && 'paginator__disabled';
-
-  return (
-    <div className="paginator">
-      <Link
-        className={prevDisabledClassName}
-        to={`${route}?page=${Number(page) - 1}`}
-        onClick={onClick}
-      >
-        <span className={prevDisabledClassName}>Prev</span>
-      </Link>
-      {` | ${page} | `}
-      <Link
-        className={nextDisabledClassName}
-        to={`${route}?page=${Number(page) + 1}`}
-        onClick={onClick}
-      >
-        <span className={nextDisabledClassName}>More</span>
-      </Link>
-    </div>
-  );
-};
+}
 
 export default Paginator;
