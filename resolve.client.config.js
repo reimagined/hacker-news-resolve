58e3c335f6ed00b38acf9f653a0ef8451979e1ec
Vasiliy-Lavrov <29576033+Vasiliy-Lavrov@users.noreply.github.com>
2017-09-13 09:50:34 +0300

    Refactor root component

--- resolve.client.config.js
+++ resolve.client.config.js
@@ -1,15 +1,7 @@
-import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
-
-import RouteWithSubRoutes from './client/components/RouteWithSubRoutes'
+import { clientRootComponent } from './client/components/App'
 import createStore from './client/store'
-import routes from './client/routes'
 
 export default {
-  createStore,
-  rootComponent: () => (
-    <BrowserRouter>
-      <RouteWithSubRoutes routes={routes} />
-    </BrowserRouter>
-  )
+  rootComponent: clientRootComponent,
+  createStore
 }
