f4fa715d2b58ec60159474ce4a385974088f6ad2
timbset <timophey.trainenkov@devexpress.com>
2017-10-16 15:34:48 +0300

    Fix fetch timeout

--- import/api.js
+++ import/api.js
@@ -1,15 +1,31 @@
 import fetch from 'isomorphic-fetch'
 
 const endPoint = 'https://hacker-news.firebaseio.com/v0'
+const timeout = 10000
+
+const wait = (time, result) =>
+  new Promise(resolve => setTimeout(() => resolve(result), time))
 
 const fetchWithRetry = async (url, retry = 0) => {
   try {
-    return await fetch(url, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json'
-      }
-    })
+    const waitResult = {}
+
+    const response = await Promise.race([
+      fetch(url, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json'
+        }
+      }),
+      wait(timeout, waitResult)
+    ])
+
+    if (response === waitResult) {
+      console.error(`Fetch timeout (${timeout} ms)`)
+      process.exit(1)
+    }
+
+    return response
   } catch (e) {
     if (retry <= 3) {
       return fetchWithRetry(url, retry + 1)
