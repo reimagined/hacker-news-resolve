b9f846e7b0ab114520eb5ff6e5f2bf8b374669a8
timbset <timophey.trainenkov@devexpress.com>
2017-10-16 14:28:47 +0300

    Simplify importer

--- import/importer.js
+++ import/importer.js
@@ -15,24 +15,25 @@ const {
   COMMENT_CREATED
 } = eventTypes
 
-const addEvent = (type, aggregateId, timestamp, payload) =>
-  eventStore.saveEventRaw({
-    type,
-    aggregateId,
-    timestamp,
-    payload
-  })
-
 const generateUserEvents = name => {
   const aggregateId = uuid.v4()
-  addEvent(USER_CREATED, aggregateId, USER_CREATED_TIMESTAMP, { name })
+
+  eventStore.saveEventRaw({
+    type: USER_CREATED,
+    aggregateId,
+    timestamp: USER_CREATED_TIMESTAMP,
+    payload: { name }
+  })
+
   users[name] = aggregateId
   return aggregateId
 }
 
-const fetchUser = userName => {
-  if (users[userName]) {
-    return users[userName]
+const getUserId = userName => {
+  const user = users[userName]
+
+  if (user) {
+    return user
   }
 
   const aggregateId = generateUserEvents(userName)
@@ -41,36 +42,41 @@ const fetchUser = userName => {
 }
 
 const generateCommentEvents = (comment, aggregateId, parentId) => {
-  const userId = fetchUser(comment.by)
+  const userId = getUserId(comment.by)
   const commentId = uuid.v4()
 
-  addEvent(COMMENT_CREATED, aggregateId, comment.time * 1000, {
-    userId,
-    text: comment.text,
-    commentId,
-    parentId
+  eventStore.saveEventRaw({
+    type: COMMENT_CREATED,
+    aggregateId,
+    timestamp: comment.time * 1000,
+    payload: {
+      userId,
+      text: comment.text,
+      commentId,
+      parentId
+    }
   })
 
   return commentId
 }
 
-const commentProc = async (comment, aggregateId, parentId) => {
+const generateComment = async (comment, aggregateId, parentId) => {
   const commentId = generateCommentEvents(comment, aggregateId, parentId)
 
   if (comment.kids) {
-    await commentsProc(comment.kids, aggregateId, commentId)
+    await generateComments(comment.kids, aggregateId, commentId)
   }
 
   return aggregateId
 }
 
-async function commentsProc(ids, aggregateId, parentId) {
+async function generateComments(ids, aggregateId, parentId) {
   const comments = await api.fetchItems(ids)
   return comments.reduce(
     (promise, comment) =>
       promise.then(
         comment && comment.by
-          ? commentProc(comment, aggregateId, parentId)
+          ? generateComment(comment, aggregateId, parentId)
           : null
       ),
     Promise.resolve()
@@ -79,35 +85,67 @@ async function commentsProc(ids, aggregateId, parentId) {
 
 const generatePointEvents = (aggregateId, pointCount) => {
   const keys = Object.keys(users)
-  for (let i = 0; i < Math.min(keys.length, pointCount); i++) {
-    addEvent(STORY_UPVOTED, aggregateId, Date.now(), {
-      userId: users[keys[i]]
+  const count = Math.min(keys.length, pointCount)
+
+  for (let i = 0; i < count; i++) {
+    eventStore.saveEventRaw({
+      type: STORY_UPVOTED,
+      aggregateId,
+      timestamp: Date.now(),
+      payload: {
+        userId: users[keys[i]]
+      }
     })
   }
 }
 
 const generateStoryEvents = async story => {
-  if (story && story.by) {
-    const aggregateId = uuid.v4()
-    const userId = fetchUser(story.by)
+  if (!story || !story.by) {
+    return
+  }
 
-    addEvent(STORY_CREATED, aggregateId, story.time * 1000, {
+  const aggregateId = uuid.v4()
+
+  eventStore.saveEventRaw({
+    type: STORY_CREATED,
+    aggregateId,
+    timestamp: story.time * 1000,
+    payload: {
       title: story.title,
       text: story.text,
-      userId,
+      userId: getUserId(story.by),
       link: story.url
-    })
-
-    if (story.score) {
-      generatePointEvents(aggregateId, story.score)
     }
+  })
 
-    if (story.kids) {
-      await commentsProc(story.kids, aggregateId, aggregateId)
-    }
-
-    return aggregateId
+  if (story.score) {
+    generatePointEvents(aggregateId, story.score)
   }
+
+  if (story.kids) {
+    await generateComments(story.kids, aggregateId, aggregateId)
+  }
+
+  return aggregateId
+}
+
+const getUniqueStoryIds = categories => {
+  const result = categories.reduce((set, ids) => {
+    ids.forEach(id => set.add(id))
+    return set
+  }, new Set())
+
+  return [...result]
+}
+
+const fetchStoryIds = async () => {
+  const categories = await Promise.all(
+    ['topstories', 'newstories', 'showstories', 'askstories'].map(category =>
+      api.fetchStoryIds(category)
+    )
+  )
+
+  return getUniqueStoryIds(categories)
 }
 
 const fetchStories = async (ids, tickCallback) => {
@@ -124,22 +162,6 @@ const fetchStories = async (ids, tickCallback) => {
       }),
     Promise.resolve()
   )
-}
-
-const fetchStoryIds = async () => {
-  const categories = await Promise.all([
-    api.fetchStoryIds('topstories'),
-    api.fetchStoryIds('newstories'),
-    api.fetchStoryIds('showstories'),
-    api.fetchStoryIds('askstories')
-  ])
-
-  const resultSet = categories.reduce((set, ids) => {
-    ids.forEach(id => set.add(id))
-    return set
-  }, new Set())
-
-  return [...resultSet]
 }
 
 export const start = async (countCallback, tickCallback) => {
