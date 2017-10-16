9926b127e93e87e626ce730d3d443332157d8bb9
timbset <timophey.trainenkov@devexpress.com>
2017-10-16 16:47:23 +0300

    Improve read-model exports

---
+++ common/read-models/graphql/index.js
@@ -0,0 +1,5 @@
+import collections from './collections'
+import resolvers from './resolvers'
+import schema from './schema'
+
+export { collections, resolvers, schema }
--- resolve.server.config.js
+++ resolve.server.config.js
@@ -6,9 +6,13 @@ import createStore from './client/store'
 import aggregates from './common/aggregates'
 import * as events from './common/events'
 import createMemoryAdapter from './common/read-models/createMemoryAdapter'
-import gqlCollections from './common/read-models/graphql/collections'
-import gqlSchema from './common/read-models/graphql/schema'
-import gqlResolvers from './common/read-models/graphql/resolvers'
+
+import {
+  collections as gqlCollections,
+  resolvers as gqlResolvers,
+  schema as gqlSchema
+} from './common/read-models/graphql'
+
 import { extendExpress, initialState } from './server'
 
 import {
