47f6e9bf5f8d11ec98a8362df8bcf323c00eba51
Kirill Babich <kirill.babich@mail.ru>
2017-10-19 14:54:17 +0300

    Extract validation (#80)
    * Extract validation
    
    * Small fixes
    
    * Fix coverage
    
    * Fix stateExists

---
+++ common/aggregates/validation.js
@@ -0,0 +1,37 @@
+export default {
+  stateExists: (state, type) => {
+    if (!state || Object.keys(state).length === 0) {
+      throw new Error(`${type} does not exist`)
+    }
+  },
+
+  stateIsAbsent: (state, type) => {
+    if (Object.keys(state).length > 0) {
+      throw new Error(`${type} already exists`)
+    }
+  },
+
+  fieldRequired: (payload, field) => {
+    if (!payload[field]) {
+      throw new Error(`The "${field}" field is required`)
+    }
+  },
+
+  userNotVoted: (state, userId) => {
+    if (state.voted.includes(userId)) {
+      throw new Error('User already voted')
+    }
+  },
+
+  userVoted: (state, userId) => {
+    if (!state.voted.includes(userId)) {
+      throw new Error('User did not vote')
+    }
+  },
+
+  commentNotExists: (state, commentId) => {
+    if (state.comments[commentId]) {
+      throw new Error('Comment already exists')
+    }
+  }
+}
