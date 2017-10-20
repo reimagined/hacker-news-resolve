a067703fa501faec029d75d9e86b248256a160d0
mrcheater <slayerx@narod.ru>
2017-10-20 16:39:18 +0300

    Use gqlConnector from resolve-redux for NewestByPage / Refactor readModels config

--- client/components/App.js
+++ client/components/App.js
@@ -1,22 +1,13 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { StaticRouter } from 'react-router'
-import { ApolloProvider } from 'react-apollo'
-import ApolloClient, { createNetworkInterface } from 'apollo-client'
-
 import RouteWithSubRoutes from './RouteWithSubRoutes'
 import routes from '../routes'
 
-const networkInterface = createNetworkInterface({ uri: '/api/query/graphql' })
-const client = new ApolloClient({ networkInterface })
 const isClient = typeof window === 'object'
 
 const App = () => {
-  const children = (
-    <ApolloProvider client={client}>
-      <RouteWithSubRoutes routes={routes} />
-    </ApolloProvider>
-  )
+  const children = <RouteWithSubRoutes routes={routes} />
 
   return isClient ? (
     <BrowserRouter>{children}</BrowserRouter>
--- common/read-models/graphql/index.js
+++ common/read-models/graphql/index.js
@@ -1,5 +1,9 @@
-import collections from './collections'
-import resolvers from './resolvers'
-import schema from './schema'
+import projection from './collections'
+import gqlResolvers from './resolvers'
+import gqlSchema from './schema'
 
-export { collections, resolvers, schema }
+export default {
+  projection,
+  gqlSchema,
+  gqlResolvers
+}
