a1ac159e70dc09b6ace705472e9bf18f48b7e2a5
mrcheater <slayerx@narod.ru>
2018-01-12 12:17:29 +0300

    Fix #124

--- import/api.js
+++ import/api.js
@@ -1,23 +1,37 @@
 import fetch from 'isomorphic-fetch'
+import dns from 'dns'
 
-const endPoint = 'https://hacker-news.firebaseio.com/v0'
 const timeout = 15000
+
+const firebaseIP = new Promise((resolve, reject) => {
+  dns.resolve4('hacker-news.firebaseio.com', (err, addresses) => {
+    if (err) {
+      return reject(err)
+    }
+    resolve(addresses[0])
+  })
+})
 
 const wait = (time, result) =>
   new Promise(resolve => setTimeout(() => resolve(result), time))
 
 const fetchSingle = url =>
-  fetch(url, {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json'
-    }
-  }).then(response => {
-    if (!response.ok) {
-      throw new Error(response.text())
-    }
-    return response.json()
-  })
+  firebaseIP
+    .then(ip =>
+      fetch(`https://${ip}/v0/${url}`, {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Host: 'hacker-news.firebaseio.com'
+        }
+      })
+    )
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.text())
+      }
+      return response.json()
+    })
 
 const fetchWithRetry = url => {
   return Promise.race([
@@ -36,9 +50,9 @@ const fetchWithRetry = url => {
   ])
 }
 
-const fetchStoryIds = path => fetchWithRetry(`${endPoint}/${path}.json`)
+const fetchStoryIds = path => fetchWithRetry(`${path}.json`)
 
-const fetchItem = id => fetchWithRetry(`${endPoint}/item/${id}.json`)
+const fetchItem = id => fetchWithRetry(`item/${id}.json`)
 
 const fetchItems = ids => {
   return Promise.all(ids.map(fetchItem))
